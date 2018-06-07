8e527b43cfd549cfbe73862dc686e833d3bf080f
m1ya <o_izy@yahoo.co.jp>
2018-06-07 18:44:32 +0900

    feat: insert data to db

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const client = require('cheerio-httpcli');
-const sqlite = require('sqlite');
-const dbPromise = sqlite.open('./db/payment.db', { Promise });
+var sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/pokemon.db');
 
 // yakkunにアクセス
 client.fetch('https://yakkun.com/sm/zukan/', (err, $, res) => {
 
+	const stmt = db.prepare("INSERT INTO pokemon(name, h, a, b, c, d, s) VALUES (?, ?, ?, ?, ?, ?, ?)");
 	let count = 0;
     // ポケモン一覧を抜き出す
     $('ul.pokemon_list a').each(function (idx) {
     	if(count < 2){
     		const name = $(this).text();
-        	client.fetch('https://yakkun.com' + $(this).attr("href"), { q: 'node.js' }, (err, $, res) => {
+        	client.fetch('https://yakkun.com' + $(this).attr("href"), (err, $, res) => {
 
         		// 名前を表示
         		console.log(name);
+
+        		const bs = [];
 
         		// データを表示
 			    $('table.center tr').each(function (idx) {
@@ -23,9 +26,12 @@ client.fetch('https://yakkun.com/sm/zukan/', (err, $, res) => {
 			    	if($(this).text().match(/平均/)){
 			    		return false;
 			    	}
+
+			    	bs.push($(this).text().split(/\s/)[1]);
 			    	console.log($(this).text().split(/\s/)[0] + " is " + $(this).text().split(/\s/)[1]);
 
 			    });
+			    stmt.run(name, bs[0], bs[1], bs[2], bs[3], bs[4], bs[5]);
 			});
         	count++;
     	}
