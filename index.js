af1e99cacd548bc6342802564ed6bf4535c12294
m1ya <o_izy@yahoo.co.jp>
2018-06-07 17:44:43 +0900

    fix: no function & use arrow

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const client = require('cheerio-httpcli');
+const sqlite = require('sqlite');
+const dbPromise = sqlite.open('./db/payment.db', { Promise });
 
-// Googleで「node.js」について検索する。
-client.fetch('https://yakkun.com/sm/zukan/', { q: 'node.js' }, function (err, $, res) {
+// yakkunにアクセス
+client.fetch('https://yakkun.com/sm/zukan/', (err, $, res) => {
 
 	let count = 0;
     // ポケモン一覧を抜き出す
     $('ul.pokemon_list a').each(function (idx) {
     	if(count < 2){
     		const name = $(this).text();
-        	client.fetch('https://yakkun.com' + $(this).attr("href"), { q: 'node.js' }, function (err, $, res) {
+        	client.fetch('https://yakkun.com' + $(this).attr("href"), { q: 'node.js' }, (err, $, res) => {
 
         		// 名前を表示
         		console.log(name);
@@ -21,7 +23,7 @@ client.fetch('https://yakkun.com/sm/zukan/', { q: 'node.js' }, function (err, $,
 			    	if($(this).text().match(/平均/)){
 			    		return false;
 			    	}
-			    	console.log($(this).text().split(/\s/)[0] + " is " + ($(this).text().split(/\s/)[1]));
+			    	console.log($(this).text().split(/\s/)[0] + " is " + $(this).text().split(/\s/)[1]);
 
 			    });
 			});
